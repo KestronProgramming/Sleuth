05c5afe5a3e791f2d114b99c3a3467819f3c989f
Kestron <98848004+SMOMusic@users.noreply.github.com>
2022-11-24 13:03:40 -0800

    Add files via upload

---
+++ eastereggs.js
@@ -0,0 +1,46 @@
+let egg=``;
+let date=new Date();
+const daysOfTheWeek=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+function isTime(m,d){
+	let day=date.getDate();
+	let mon=date.getMonth()+1;
+	if(m&&!d){
+		switch(m){
+			case 'thanksgiving':
+				let weekday=daysOfTheWeek[date.getDay()];
+				if(weekday==="Thursday"){
+					let counter=0;
+					for(var i=day;i>-1;i-=7){
+						counter++;
+					}
+					if(counter===4&&mon===11){
+						return true;
+					}
+				}
+			break;
+			case 'christmas':
+			break;
+			case 'harvest':
+			break;
+			case 'newyears':
+			break;
+			case 'valentines':
+			break;
+			case 'stpatrick':
+			break;
+			case 'mothers':
+			break;
+			case 'fathers':
+			break;
+			case '4thjuly':
+			break;
+			case 'easter':
+			break;
+		}
+	}
+	return date.getMinutes()===m&&date.getDay()===d;
+}
+if(isTime("thanksgiving")){
+	egg=`<img src='turkey.png' class='egg'>`;
+}
+document.body.innerHTML+=egg;
